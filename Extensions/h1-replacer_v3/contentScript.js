91b0bccb0ba3d76a22071587e80b1c3d1ab002ce
showloser <ngweiming2004@gmail.com>
2023-06-15 13:00:31 +0800

    chatgpt v3 h1 replacer works

--- Extensions/h1-replacer_v3/contentScript.js
+++
@@ -1,5 +0,0 @@
-console.log('line')
-var s = `<img src=x onerror=alert(1)>`
-document.write(s)
-var replacementInput = document.getElementById('replacementInput');
-var tags = document.getElementsByTagName('h1')
